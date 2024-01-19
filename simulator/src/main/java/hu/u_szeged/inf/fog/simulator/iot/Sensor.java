6dc6143b435c94d0c28c61154b2fd1ba7519559b
Markus Andras <markusa@inf.u-szeged.hu>
2024-01-19 16:17:34 +0100

    javadoc added to Sensor.java

--- simulator/src/main/java/hu/u_szeged/inf/fog/simulator/iot/Sensor.java
+++ simulator/src/main/java/hu/u_szeged/inf/fog/simulator/iot/Sensor.java
@@ -6,17 +6,38 @@ import hu.mta.sztaki.lpds.cloud.simulator.io.StorageObject;
 import hu.u_szeged.inf.fog.simulator.util.TimelineVisualiser.TimelineEntry;
 import java.util.ArrayList;
 
+/**
+ * This class is an abstract representation of an IoT sensor, its only task
+ * is to create a file with a given delay as a result of a measurement.
+ */
 public class Sensor extends DeferredEvent {
 
+    /**
+     * It contains the log of a sensor events. 
+     * It is used only for visualization, in IoT workflow simulations.
+     */
     public static ArrayList<TimelineEntry> sensorEventList = new ArrayList<>();
 
+    /**
+     * The device that utilizes this sensor. 
+     */
     private Device device;
 
+    /**
+     * Defines a sensor measurement.
+     * @param device the device that will store the generated data
+     * @param delay the length of the measurement (ms)
+     */
     Sensor(Device device, long delay) {
         super(delay);
         this.device = device;
     }
 
+    /**
+     * It creates and saves the file in the storage of the device.
+     * If the data cannot be save (e.g. due to lack of space), 
+     * the simulation terminates.
+     */
     @Override
     protected void eventAction() {
         StorageObject so = new StorageObject(
