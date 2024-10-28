79bff15b12d3d66ec26d6cc302306821e8d6b0a2
ceze88 <dev.ceze@gmail.com>
2024-10-28 12:32:15 +0100

    Extend user molder

--- webapp/frontend/src/app/models/user.ts
+++ webapp/frontend/src/app/models/user.ts
@@ -2,4 +2,10 @@ export interface User {
   email: string;
   password: string;
   university: string;
+  simulationsRun: number;   //Number of simulations run in the current period
+  maxSimulations: number;   //Max number of simulations that can be run in the current period
+  totalRuntime: number;     //Total runtime in the current period in seconds
+  maxRuntime: number;       //Maximum runtime in the current period in seconds
+  resetPeriod: number;      //Reset period in days
+  lastReset: number;        //Timestamp of the last reset
 }
