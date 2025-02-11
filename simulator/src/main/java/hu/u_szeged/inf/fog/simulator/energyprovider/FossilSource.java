27f3053582aee8bdd6c5d156120ae1a2246fc3c4
Balogh Gellert <h144133@stud.u-szeged.hu>
2025-02-11 12:01:56 +0100

    making fossil source non burnt in

--- simulator/src/main/java/hu/u_szeged/inf/fog/simulator/energyprovider/FossilSource.java
+++ simulator/src/main/java/hu/u_szeged/inf/fog/simulator/energyprovider/FossilSource.java
@@ -2,12 +2,15 @@ package hu.u_szeged.inf.fog.simulator.energyprovider;
 
 public class FossilSource extends EnergySource{
 
-    FossilSource() {
+    float production;
+
+    FossilSource(float production) {
         super(false);
+        this.production = production;
     }
 
     @Override
     float Production(long time, long frequency) {
-        return 99999999;
+        return this.production;
     }
 }
