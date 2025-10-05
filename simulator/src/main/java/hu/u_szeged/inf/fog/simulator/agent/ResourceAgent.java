91eed0cf8fe6fda0d5680612b4e2c446c4ee8b76
kovacsmate317 <kovacsmate317@gmail.com>
2025-10-05 16:47:42 +0200

    trying to revert bad predictor-ui, merge new master

--- simulator/src/main/java/hu/u_szeged/inf/fog/simulator/agent/ResourceAgent.java
+++ simulator/src/main/java/hu/u_szeged/inf/fog/simulator/agent/ResourceAgent.java
@@ -66,14 +66,13 @@ public class ResourceAgent {
     int callcounter;
 
     public ResourceAgent(String name, double hourlyPrice, VirtualAppliance resourceAgentVa,
-                         AlterableResourceConstraints resourceAgentArc, AgentStrategy agentStrategy, MessagingStrategy messagingStrategy, Capacity... capacities) {
+                         AlterableResourceConstraints resourceAgentArc, AgentStrategy agentStrategy, MessagingStrategy messagingStrategy) {
         this.capacities = new ArrayList<>();
-        validateAndAddCapacitiesLimit(Arrays.asList(capacities));
         this.name = name;
         this.hourlyPrice = hourlyPrice;
         ResourceAgent.resourceAgents.add(this);
         this.agentStrategy = agentStrategy;
-        //this.initResourceAgent(resourceAgentVa, resourceAgentArc);
+        //this.initResourceAgent(resourceAgentVa, resourceAgentArc)
         this.messagingStrategy = messagingStrategy;
     }
 
