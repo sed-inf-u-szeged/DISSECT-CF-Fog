e8ea0eb5051f8329bc658e0d82ea0a713dc7b88f
Szász Gergő Péter <h378060@stud.u-szeged.hu>
2025-11-01 00:18:30 +0100

    Battery bug fix, example for idle drain

---
+++ simulator/src/main/java/hu/u_szeged/inf/fog/simulator/demo/simple/IdleBatteryDrainExample.java
@@ -0,0 +1,28 @@
+package hu.u_szeged.inf.fog.simulator.demo.simple;
+
+import hu.mta.sztaki.lpds.cloud.simulator.Timed;
+import hu.u_szeged.inf.fog.simulator.demo.ScenarioBase;
+import hu.u_szeged.inf.fog.simulator.iot.Battery;
+
+import hu.u_szeged.inf.fog.simulator.util.AgentVisualiser;
+
+import java.io.File;
+import java.nio.file.Path;
+
+
+
+public class IdleBatteryDrainExample {
+    public static void main(String[] args) {
+        //drainRate jelenleg változásig mAh/h érték tehát kivonás előtt van egy 60-as osztó tehát a szimulációhoz nem túl reális "idle drain" értéket adtam meg mert amúgy órákat kell szimulálni
+        Battery battery1 = new Battery("battery1", 3500, 4.4f, 200, 2 * 60 * 60 * 1000);
+        Battery battery2 = new Battery("battery2", 4500, 4.4f, 200, 3 * 60 * 60 * 1000);
+
+        //48 hour
+        Timed.simulateUntil(48 * 60 * 60 * 1000);
+
+        battery1.writeToFileConsumption(ScenarioBase.resultDirectory);
+        battery2.writeToFileConsumption(ScenarioBase.resultDirectory);
+
+        AgentVisualiser.visualise("batteryGraph", Path.of(ScenarioBase.resultDirectory + File.separator + "battery1.csv"), Path.of(ScenarioBase.resultDirectory + File.separator + "battery2.csv"));
+    }
+}
