07169721c12c5e8d1b1ead2aeb1a74647f415181
Szász Gergő Péter <h378060@stud.u-szeged.hu>
2026-01-08 11:10:11 +0100

    selectBestCommunicationProtocol() structure, communication protocols kind of done (but placeholder values)

--- simulator/src/main/java/hu/u_szeged/inf/fog/simulator/iot/CommunicationProtocol.java
+++ simulator/src/main/java/hu/u_szeged/inf/fog/simulator/iot/CommunicationProtocol.java
@@ -8,48 +8,55 @@ import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 
+//singleton osztály amivel generálhatunk repokat az egyes kommunikációs protokollokhoz
 public class CommunicationProtocol {
-    //enumba nem megoldható de enumos naming convention mert szerepükben azok
-    public static int WiFiRepoCounter = 1;
-    public static int _5GRepoCounter = 1;
-    public static int LoRaRepoCounter = 1;
-    public final Repository WIFI;
-    public final Repository _5G;
-    public final Repository LORA;
+    private static CommunicationProtocol instance = null;
 
-    CommunicationProtocol() {
-        this.WIFI = makeWifiRepository();
-        this._5G = make5GRepository();
-        this.LORA = makeLoRaRepository();
+    private static int WiFiRepoCounter = 1;
+    private static int _5GRepoCounter = 1;
+    private static int LoRaRepoCounter = 1;
+
+    private CommunicationProtocol(){}
+
+    public static CommunicationProtocol getInstance() {
+        if (instance == null)
+            instance = new CommunicationProtocol();
+
+        return instance;
     }
 
-    private Repository makeWifiRepository(){
+
+    /*
+    az egyes értékeken még dolgozni kell, mert jelenleg még nem találtam megfelelő forrást amiből egyértelmű adatok jönnének le szóval
+    egyelőre a legtöbb dolog placeholder és nincs sok értelmük főleg, hogy a maxpowert nem is tudom hogy kéne belőni mert szinte sose láttom csak az idlet
+    */
+    public Repository newWifiRepository(){
         EnumMap<PowerTransitionGenerator.PowerStateKind, Map<String, PowerState>> transitions =
-                PowerTransitionGenerator.generateTransitions(0.065, 1.475, 2.0, 1, 2);
+                PowerTransitionGenerator.generateTransitions(0.065, 1.55, 2.0, 1, 2);
 
         final Map<String, PowerState> stTransitions = transitions.get(PowerTransitionGenerator.PowerStateKind.storage);
         final Map<String, PowerState> nwTransitions = transitions.get(PowerTransitionGenerator.PowerStateKind.network);
 
-        return new Repository(4_294_967_296L, "WIFI-Repo" + WiFiRepoCounter++, 1,1,1, new HashMap<>(), stTransitions, nwTransitions);
+        return new Repository(4_294_967_296L, "WIFI-Repo" + WiFiRepoCounter++, 18750,18750,18750, new HashMap<>(), stTransitions, nwTransitions); //150 Mbit/s
     }
 
-    private Repository make5GRepository(){
+    public Repository new5GRepository(){
         EnumMap<PowerTransitionGenerator.PowerStateKind, Map<String, PowerState>> transitions =
-                PowerTransitionGenerator.generateTransitions(0.065, 1.475, 2.0, 1, 2);
+                PowerTransitionGenerator.generateTransitions(0.065, 1.65, 2.0, 1, 2);
 
         final Map<String, PowerState> stTransitions = transitions.get(PowerTransitionGenerator.PowerStateKind.storage);
         final Map<String, PowerState> nwTransitions = transitions.get(PowerTransitionGenerator.PowerStateKind.network);
 
-        return new Repository(4_294_967_296L, "5G-Repo" + _5GRepoCounter++, 1,1,1, new HashMap<>(), stTransitions, nwTransitions);
+        return new Repository(4_294_967_296L, "5G-Repo" + _5GRepoCounter++, 12500,12500,12500, new HashMap<>(), stTransitions, nwTransitions); //100 Mbit/s
     }
 
-    private Repository makeLoRaRepository(){
+    public Repository newLoRaRepository(){
         EnumMap<PowerTransitionGenerator.PowerStateKind, Map<String, PowerState>> transitions =
-                PowerTransitionGenerator.generateTransitions(0.065, 1.475, 2.0, 1, 2);
+                PowerTransitionGenerator.generateTransitions(0.045, 1.45, 2.0, 1, 2);
 
         final Map<String, PowerState> stTransitions = transitions.get(PowerTransitionGenerator.PowerStateKind.storage);
         final Map<String, PowerState> nwTransitions = transitions.get(PowerTransitionGenerator.PowerStateKind.network);
 
-        return new Repository(4_294_967_296L, "LORA-Repo" + LoRaRepoCounter++, 1,1,1, new HashMap<>(), stTransitions, nwTransitions);
+        return new Repository(4_294_967_296L, "LORA-Repo" + LoRaRepoCounter++, 1,1,1, new HashMap<>(), stTransitions, nwTransitions); //8 Kbit/s?
     }
 }
